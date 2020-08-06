976ed3d5d7edf6e6ef3c37b15431fc33322091fa
Sidharth <sidharthpsnirmallur@gmail.com>
2020-08-06 19:41:42 -0400

    offer admin added

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Products
+from .models import Products,Offers
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name','price','stock')
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code','discount')
 # Register your models here.
 admin.site.register(Products,ProductAdmin)
+admin.site.register(Offers,OfferAdmin)
