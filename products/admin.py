cecf54702bfca4b5452c23c7f8c44061e996f5bb
Sidharth <sidharthpsnirmallur@gmail.com>
2020-08-06 19:31:23 -0400

    issues fix

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Products
-
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','price','stock')
 # Register your models here.
-admin.site.register(Products)
+admin.site.register(Products,ProductAdmin)
