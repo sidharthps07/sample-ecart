eb08ee51ec5288bcef4d2d555403edad90987904
Sidharth <sidharthpsnirmallur@gmail.com>
2020-08-06 18:49:50 -0400

    fixing issues in moduls

--- ecart/urls.py
+++ ecart/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from django.urls import include,path
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/',include('product.urls'))
+    path('products/',include('products.urls'))
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
-def index(request):
+def products(request):
     return HttpResponse("<h1>Product page</h1>")
